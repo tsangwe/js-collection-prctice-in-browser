2d44e200a780484e8cdf49a0ec7388e2b3011fc1
Wesley Tsang <tsangwe@oocl.com>
2020-04-14 01:26:56 +0800

    [Wesley] Pass session 2 partice-2

--- practice/section-2/practice-2.js
+++ practice/section-2/practice-2.js
@@ -1,5 +1,28 @@
 'use strict';
 
 function countSameElements(collection) {
-  return 'Implement the practice require, and begin changing code in this row';
+  let result = [];
+
+  collection.forEach(element => {
+    let isNewElement = true;
+    let readElement = element.split("-");
+    let key = readElement[0];
+    let amount = 1;
+
+    if (readElement.length > 1) amount = readElement[1];
+
+    result.forEach(seenElement => {
+      if (seenElement['key'] == key) {
+        seenElement['count'] += amount;
+        isNewElement = false;
+      }
+    })
+    if (isNewElement) {
+      result.push({key: key, count: parseInt(amount)});
+    }
+  });
+
+  console.log(result);
+
+  return result;
 }
