faf95c18128a48401d559fbbc829da4d85ce13ea
Wesley Tsang <tsangwe@oocl.com>
2020-04-13 18:11:30 +0800

    [Wesley] Pass session 1 practice-2

--- practice/section-1/practice-2.js
+++ practice/section-1/practice-2.js
@@ -3,15 +3,15 @@
 function collectSameElements(collectionA, collectionB) {
   var result = [];
 
-  // for (var i = 0; i < collectionA.length; i++) {
-  //   for (var j = 0; j < collectionB.length; j++) {
-  //     for (var k = 0; k < collectionB[j].length; k++) {
-  //       if (collectionA[i] == collectionB[j][k]) {
-  //         result.push(collectionA[i]);
-  //       }
-  //     }
-  //   }
-  // }
+  for (var i = 0; i < collectionA.length; i++) {
+    for (var j = 0; j < collectionB.length; j++) {
+      for (var k = 0; k < collectionB[j].length; k++) {
+        if (collectionA[i] == collectionB[j][k]) {
+          result.push(collectionA[i]);
+        }
+      }
+    }
+  }
 
   return result;
 }
