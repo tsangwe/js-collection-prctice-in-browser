6193934df4335f9ab66b6d530569e5849850e075
Wesley Tsang <tsangwe@oocl.com>
2020-04-13 18:16:07 +0800

    [Wesley] Refactor session 1 practice-2

--- practice/section-1/practice-2.js
+++ practice/section-1/practice-2.js
@@ -3,15 +3,15 @@
 function collectSameElements(collectionA, collectionB) {
   var result = [];
 
-  for (var i = 0; i < collectionA.length; i++) {
-    for (var j = 0; j < collectionB.length; j++) {
-      for (var k = 0; k < collectionB[j].length; k++) {
-        if (collectionA[i] == collectionB[j][k]) {
-          result.push(collectionA[i]);
+  collectionA.forEach(elementFromA => {
+    collectionB.forEach(innerArrayFromB => {
+      innerArrayFromB.forEach(elementFromInnerB => {
+        if (elementFromA == elementFromInnerB) {
+          result.push(elementFromA);
         }
-      }
-    }
-  }
+      })
+    })
+  });
 
   return result;
 }
